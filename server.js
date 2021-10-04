a6151f05c5c508311e32c2928032c732aaa72072
ktodoran <ktodoran@gmail.com>
2021-10-04 15:32:25 -0400

    Fixed package.json

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/letsgetsocial', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Lets-Get-Social', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
